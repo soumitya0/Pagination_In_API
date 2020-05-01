62997508cbbbba6497a71fc22850db62166b2d75
soumitya <soumityachauhan@gmail.com>
2020-05-01 17:41:56 +0530

    middleware to get both user model and post model

--- index.js
+++ index.js
@@ -28,33 +28,72 @@ const users = [
   { id: 24, name: "user 24" },
 ];
 
-app.get("/users", (req, res) => {
-  const page = parseInt(req.query.page);
-  const limit = parseInt(req.query.limit);
+const post = [
+  { id: 1, name: "post 1" },
+  { id: 2, name: "post 2" },
+  { id: 3, name: "post 3" },
+  { id: 5, name: "post 5" },
+  { id: 6, name: "post 6" },
+  { id: 4, name: "post 4" },
+  { id: 7, name: "post 7" },
+  { id: 8, name: "post 8" },
+  { id: 9, name: "post 9" },
+  { id: 10, name: "post 10" },
+  { id: 11, name: "post 11" },
+  { id: 12, name: "post 12" },
+  { id: 13, name: "post 13" },
+  { id: 14, name: "post 14" },
+  { id: 15, name: "post 15" },
+  { id: 16, name: "post 16" },
+  { id: 17, name: "post 17" },
+  { id: 18, name: "post 18" },
+  { id: 19, name: "post 19" },
+  { id: 20, name: "post 20" },
+  { id: 21, name: "post 21" },
+  { id: 22, name: "post 22" },
+  { id: 23, name: "post 23" },
+  { id: 24, name: "post 24" },
+];
 
-  const startIndex = (page - 1) * limit;
-  const endIndex = page * limit;
-
-  const results = {};
-
-  if (endIndex < users.length) {
-    results.nextPage = {
-      page: page + 1,
-      limit: limit,
-    };
-  }
-
-  if (startIndex > 0) {
-    results.previousPage = {
-      page: page - 1,
-      limit: limit,
-    };
-  }
-
-  results.userData = users.slice(startIndex, endIndex); // this will look like { userData [ {}, {} , {} ,] }
-
-  res.json(results);
+app.get("/post", paginatedMiddleware(post), (req, res) => {
+  res.json(res.paginatedMiddlewareResult);
 });
+
+app.get("/users", paginatedMiddleware(users), (req, res) => {
+  res.json(res.paginatedMiddlewareResult);
+});
+
+function paginatedMiddleware(model) {
+  return (req, res, next) => {
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const results = {};
+
+    if (endIndex < model.length) {
+      results.nextPage = {
+        page: page + 1,
+        limit: limit,
+      };
+    }
+
+    if (startIndex > 0) {
+      results.previousPage = {
+        page: page - 1,
+        limit: limit,
+      };
+    }
+
+    results.resultData = model.slice(startIndex, endIndex); // this will look like { userData [ {}, {} , {} ,] }
+
+    res.paginatedMiddlewareResult = results; //save this so that we can access in api's
+
+    next();
+  };
+}
 
 app.listen(5550, () => {
   console.log("i am 5550");
